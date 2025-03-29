6403b143a9e5f357b4864c471b12e6d03525a8a1
Almer Leo Almazan <almerleoalmazan@gmail.com>
2025-03-29 21:17:24 +0800

    fix: api items with search keyword

--- server/routes/api/shared/items.js
+++ server/routes/api/shared/items.js
@@ -10,12 +10,29 @@ const router = express.Router();
  * @route GET /api/shared/items
  */
 router.get('/', async (req, res) => {
-    const sql = 'SELECT * FROM items'
+    // Get the category ID from the request
+    const { keyword } = req.query;
+    console.log('keyword', keyword)
 
-    db.query(sql, (err, results) => {
+    // Ensure keyword is not undefined
+    if (!keyword) {
+        return res.status(400).json({
+            success: false,
+            message: "Keyword is required"
+        });
+    }
+
+    const sql = `
+        SELECT *
+        FROM items
+        WHERE name LIKE ?
+          AND is_approved = 1
+          AND is_archived = 0
+    `
+
+    db.query(sql, [`%${keyword}%`], (err, results) => {
         if (err) {
             console.error("Database not connected", err);
-
             return res.status(500).json({
                 success: false,
                 message: "Query failed."
