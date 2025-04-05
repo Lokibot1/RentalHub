c3d780165f4fcd156e475d5a1329e5c18406587d
Almer Leo Almazan <almerleoalmazan@gmail.com>
2025-04-05 22:31:03 +0800

    add average rating

--- server/routes/api/user/view-product.js
+++ server/routes/api/user/view-product.js
@@ -19,10 +19,13 @@ router.get("/posted-by/:item_id", async (req, res) => {
             CONCAT(users.first_name, ' ', users.last_name) AS owner,
             contact_number,
             social_media,
-            location
+            location,
+            AVG(reviews.rating) AS average_rating
         FROM items
                  JOIN users ON users.id = items.user_id
+                 LEFT JOIN reviews ON reviews.item_id = items.id
         WHERE items.id = ?
+        GROUP BY users.id, profile_image, users.first_name, users.last_name, contact_number, social_media, location
     `
 
     db.query(sql, [item_id], (err, results) => {
